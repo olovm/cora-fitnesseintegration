93ab4023cb1bd3002a0de0e152c387d6cfc2b81b
madde <madde@5812583f356f>
2018-05-01 07:17:27 +0000

    CORA-581, possible to check num of children in dataGroup

---
+++ src/main/java/se/uu/ub/cora/fitnesseintegration/MetadataGroupFixture.java
@@ -0,0 +1,72 @@
+package se.uu.ub.cora.fitnesseintegration;
+
+import java.util.List;
+
+import se.uu.ub.cora.clientdata.ClientDataElement;
+import se.uu.ub.cora.clientdata.ClientDataGroup;
+import se.uu.ub.cora.clientdata.ClientDataRecord;
+
+public class MetadataGroupFixture {
+
+	private String nameInData;
+	private String childDataGroupName;
+
+	public void setChildNameInData(String nameInData) {
+		this.nameInData = nameInData;
+
+	}
+
+	public int numberOfChildrenWithNameInData() {
+		int numOfMatchildChildren = 0;
+		ClientDataRecord record = RecordHolder.getRecord();
+		if (topLevelGroupExists(record)) {
+			numOfMatchildChildren = findNumOfMatchingChildren(record);
+		}
+		return numOfMatchildChildren;
+	}
+
+	private int findNumOfMatchingChildren(ClientDataRecord record) {
+		ClientDataGroup topLevelDataGroup = record.getClientDataGroup();
+		if (shouldFindChildrenInTopLevelDataGroup()) {
+			return getNumberOfMatchingChildren(topLevelDataGroup);
+		}
+		return possiblyGetNumOfMatchingChildrenFromChildDataGroup(topLevelDataGroup);
+	}
+
+	private boolean shouldFindChildrenInTopLevelDataGroup() {
+		return childDataGroupName == null;
+	}
+
+	private int possiblyGetNumOfMatchingChildrenFromChildDataGroup(
+			ClientDataGroup topLevelDataGroup) {
+		if (childDataGroupExist(topLevelDataGroup)) {
+			return getNumOfMatchingChildrenFromChildDataGroup(topLevelDataGroup);
+		}
+		return 0;
+	}
+
+	private boolean childDataGroupExist(ClientDataGroup topLevelDataGroup) {
+		return topLevelDataGroup.containsChildWithNameInData(childDataGroupName);
+	}
+
+	private int getNumOfMatchingChildrenFromChildDataGroup(ClientDataGroup topLevelDataGroup) {
+		ClientDataGroup childDataGroup = topLevelDataGroup
+				.getFirstGroupWithNameInData(childDataGroupName);
+		return getNumberOfMatchingChildren(childDataGroup);
+	}
+
+	private boolean topLevelGroupExists(ClientDataRecord record) {
+		return record != null && record.getClientDataGroup() != null;
+	}
+
+	private int getNumberOfMatchingChildren(ClientDataGroup topLevelDataGroup) {
+		List<ClientDataElement> matchingChildren = topLevelDataGroup
+				.getAllChildrenWithNameInData(nameInData);
+		return matchingChildren.size();
+	}
+
+	public void setChildDataGroup(String childDataGroup) {
+		this.childDataGroupName = childDataGroup;
+	}
+
+}
